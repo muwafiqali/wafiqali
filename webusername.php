6e3b7569cb2ce88329fca1a8dafe5eb959291000
muwafiqali <158023607+muwafiqali@users.noreply.github.com>
2024-02-04 09:37:13 +0700

    Add files via upload

---
+++ webusername.php
@@ -0,0 +1,11 @@
+<?php
+
+$nim="E32232007";
+$nama="Muwafiq Ali Luqman";
+$prodi="Teknik Komputer"; 
+echo "Selamat Praktikum : )"."</br>"; 
+echo $nim."</br>";
+echo $nama."</br>"; 
+echo $prodi;
+
+?>
